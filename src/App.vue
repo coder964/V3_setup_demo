553dcea15dd0f6e827227332057b1c3efc9c1b02
CoderHan <2879372048@qq.com>
2023-05-13 17:40:45 +0800

    引入数据

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,13 @@
+<script setup>
+  // 1.获取数据
+  import highScore from "./data/high_score.json"
+</script>
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    {{ highScore }}
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<style scoped>
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
