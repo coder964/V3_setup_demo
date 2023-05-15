ac971e1406d735815173a45cf9a99fe5e57ba679
CoderHan <2879372048@qq.com>
2023-05-15 17:01:24 +0800

    在App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <script setup> 
-  import highScore from "./data/high_score.json"
+  // import highScore from "./data/high_score.json"
+  import { ref } from "vue"
   import RoomArea from "./components/RoomArea.vue";
+
+  // 模拟在服务器获取数据(不会立刻拿到，而是会等一会儿)
+  const highScore = ref({})
+  setTimeout(() => {
+    // 因为是通过inport函数导入的，它会将high_score.json文件作为一个模块导入了，所以需要通过default属性来拿到数据
+    import("./data/high_score.json").then((res) => {
+      console.log(res.default);
+      highScore.value = res.default
+    })
+  }, 1000)
 
 </script>
 
